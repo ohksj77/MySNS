c87934018aaab92e92f28296214fc607e72d471f
ohksj77 <ohksj77@gmail.com>
2023-01-17 12:32:52 +0900

    mysql 클라이언트 접속 코드 완성

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const Promise = require('promise');
+const mysql = require('mysql2')
+
+exports.connection = mysql.createPool(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    }
+);
+
+/**
+ *
+ * @param {string} queryString
+ * @param {array} params
+ * @returns {ThenPromise<unknown> | ThenPromise}
+ */
+exports.pool = (queryString, params) => {
+    return new Promise((resolve, reject) => {
+        this.connection.query(queryString, params, (err, rows, fields) => {
+            (err) ? reject(err) : resolve(rows);
+        })
+    })
+}
